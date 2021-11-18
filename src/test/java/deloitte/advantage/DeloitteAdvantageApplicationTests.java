ccf6aeea75fa72355e7ba79ef5b0aaafe240042e
Nico Guba <nguba@mac.com>
2021-11-18 13:21:46 +0000

    First import

---
+++ src/test/java/deloitte/advantage/DeloitteAdvantageApplicationTests.java
@@ -0,0 +1,13 @@
+package deloitte.advantage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeloitteAdvantageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
