73c9abb33de9e4c08fa96a15dc83df56effe0926
Nico Guba <nguba@mac.com>
2021-12-09 19:13:08 +0000

    trying a different event type to detect api gateway params

--- layers/presentation/join/src/main/java/deloitte/advantage/api/Join.java
+++ layers/presentation/join/src/main/java/deloitte/advantage/api/Join.java
@@ -2,12 +2,14 @@ package deloitte.advantage.api;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 
 import java.util.Map;
 
-public class Join implements RequestHandler<Map<String, String>, String> {
+public class Join implements RequestHandler<APIGatewayV2HTTPEvent, Map<String, String>> {
+
     @Override
-    public String handleRequest(final Map<String, String> input, final Context context) {
-        return "Received " + input;
+    public Map<String, String> handleRequest(final APIGatewayV2HTTPEvent input, final Context context) {
+        return input.getPathParameters();
     }
 }
