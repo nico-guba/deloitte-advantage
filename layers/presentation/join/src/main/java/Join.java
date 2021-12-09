0ed4f3088dc0c9225fb1d41dfef822f8996354e9
Nico Guba <nguba@mac.com>
2021-12-09 16:51:48 +0000

    simple test to see how lambda plays with html

---
+++ layers/presentation/join/src/main/java/Join.java
@@ -0,0 +1,9 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Join implements RequestHandler<Integer, String> {
+    @Override
+    public String handleRequest(final Integer input, final Context context) {
+        return "Received " + input;
+    }
+}
