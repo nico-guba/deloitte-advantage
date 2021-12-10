1242c878cd3ff27ef8c670a18afac12402638470
Nico Guba <nguba@mac.com>
2021-12-10 10:29:03 +0000

    code cleanup

--- layers/presentation/join/src/main/java/deloitte/advantage/api/Join.java
+++ layers/presentation/join/src/main/java/deloitte/advantage/api/Join.java
@@ -12,24 +12,21 @@ import deloitte.advantage.lambda.EventFactory;
  */
 public class Join implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    EventFactory factory = new EventFactory(new ObjectMapper());
+    final EventFactory factory = new EventFactory(new ObjectMapper());
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         try {
-            String subscriptionId = getSubscriptionId(input);
+            final String subscriptionId = getSubscriptionId(input);
             final String message = "Received subscription for id=" + subscriptionId;
-            if(subscriptionId == null) {
+            if (subscriptionId == null) {
                 return factory.makeInvalidRequestResponse("subscription_id cannot be null");
             }
-            
-            switch (input.getHttpMethod()) {
-                case "POST":
-                    return factory.makeSuccessResponse(message);
-                default:
-                    context.getLogger().log("METHOD NOT HANDLED: " + input.getHttpMethod());
-                    return factory.makeErrorResponse("Unexpected method: " + input.getHttpMethod());
+
+            if ("POST".equals(input.getHttpMethod())) {
+                return factory.makeSuccessResponse(message);
             }
+            return factory.makeErrorResponse("Unexpected method: " + input.getHttpMethod());
         } catch (Exception e) {
             e.printStackTrace();
             return factory.makeErrorResponse(e);
@@ -37,7 +34,7 @@ public class Join implements RequestHandler<APIGatewayProxyRequestEvent, APIGate
     }
 
     private String getSubscriptionId(final APIGatewayProxyRequestEvent input) {
-        if(input != null && input.getPathParameters() != null) {
+        if (input != null && input.getPathParameters() != null) {
             return input.getPathParameters().get("subscription_id");
         }
         return null;
