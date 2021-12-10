464c85743ea45ef3b0d5b24036d1e91d8034f1f6
Michael Chadwick <95221541+mchadwickDD@users.noreply.github.com>
2021-12-10 14:14:09 +0000

    Add header elements to index.html (#6)
    - Wrap App.test.tsx with 'describe' block.
    - Change name in manifest to relate to project name.

--- layers/presentation/ui/src/App.test.tsx
+++ layers/presentation/ui/src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The homepage', () => {
+
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
 });
